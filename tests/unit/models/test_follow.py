f2ee1c6b7b3b85c1d46a525de70b3f97ff98bcfc
lindhjonathan <lindh.jonathan@gmail.com>
2019-12-06 15:30:16 +0100

    Updated tests to use pytest

--- tests/unit/models/test_follow.py
+++ tests/unit/models/test_follow.py
@@ -7,51 +7,92 @@ import pytest
 from app import bp, db
 from app.models import User, Post
 
-class UserModelCase(pytest.TestCase):
+class UserModelCase():
+    """
+    Test class for containing tests related to User and follower functions
+    """
     def setUp(self):
+        """
+        Set up database session
+        """
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
         db.create_all()
 
     def tearDown(self):
+        """
+        Teardown the database session
+        """
         db.session.remove()
         db.drop_all()
 
     def test_password_hashing(self):
+        """
+        Test password hashing
+        """
         u = User(username='susan')
         u.set_password('cat')
-        self.assertFalse(u.check_password('dog'))
-        self.assertTrue(u.check_password('cat'))
+        # self.assertFalse(u.check_password('dog'))
+        # self.assertTrue(u.check_password('cat'))
+
+        assert u.check_password('dog') is False
+        assert u.check_password('cat') is True
 
     def test_avatar(self):
+        """
+        Assert that the gratavar is correct for user
+        """
         u = User(username='john', email='john@example.com')
         self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
                                          'd4c74594d841139328695756648b6bd6'
                                          '?d=identicon&s=128'))
 
+        assert str(u.avatar(128)) == "https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128"
+
     def test_follow(self):
+        """
+        Test follow functionality
+        """
         u1 = User(username='john', email='john@example.com')
         u2 = User(username='susan', email='susan@example.com')
         db.session.add(u1)
         db.session.add(u2)
         db.session.commit()
-        self.assertEqual(u1.followed.all(), [])
-        self.assertEqual(u1.followers.all(), [])
+        # self.assertEqual(u1.followed.all(), [])
+        # self.assertEqual(u1.followers.all(), [])
+
+        assert u1.followed.all() == []
+        assert u1.followers.all() == []
 
         u1.follow(u2)
         db.session.commit()
-        self.assertTrue(u1.is_following(u2))
-        self.assertEqual(u1.followed.count(), 1)
-        self.assertEqual(u1.followed.first().username, 'susan')
-        self.assertEqual(u2.followers.count(), 1)
-        self.assertEqual(u2.followers.first().username, 'john')
+        # self.assertTrue(u1.is_following(u2))
+        # self.assertEqual(u1.followed.count(), 1)
+        # self.assertEqual(u1.followed.first().username, 'susan')
+        # self.assertEqual(u2.followers.count(), 1)
+        # self.assertEqual(u2.followers.first().username, 'john')
+
+        assert u1.is_following(u2) is True
+        assert u1.followed.count() == 1
+        assert u1.followed.first().username == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().username == 'john'
 
         u1.unfollow(u2)
         db.session.commit()
-        self.assertFalse(u1.is_following(u2))
-        self.assertEqual(u1.followed.count(), 0)
-        self.assertEqual(u2.followers.count(), 0)
+        # self.assertFalse(u1.is_following(u2))
+        # self.assertEqual(u1.followed.count(), 0)
+        # self.assertEqual(u2.followers.count(), 0)
+
+        assert u1.is_following(u2) is False
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
 
     def test_follow_posts(self):
+        """
+        Create users, posts and followers
+        """
         # create four users
         u1 = User(username='john', email='john@example.com')
         u2 = User(username='susan', email='susan@example.com')
@@ -84,7 +125,12 @@ class UserModelCase(pytest.TestCase):
         f2 = u2.followed_posts().all()
         f3 = u3.followed_posts().all()
         f4 = u4.followed_posts().all()
-        self.assertEqual(f1, [p2, p4, p1])
-        self.assertEqual(f2, [p2, p3])
-        self.assertEqual(f3, [p3, p4])
-        self.assertEqual(f4, [p4])
+        # self.assertEqual(f1, [p2, p4, p1])
+        # self.assertEqual(f2, [p2, p3])
+        # self.assertEqual(f3, [p3, p4])
+        # self.assertEqual(f4, [p4])
+
+        assert f1 == [p2, p4, p1]
+        assert f2 == [p2, p3]
+        assert f3 == [p3, p4]
+        assert f4 == [p4]
