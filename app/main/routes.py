56a52eb45e024a5baf7fb78aa4f857e3352f8ae7
lindhjonathan <lindh.jonathan@gmail.com>
2019-12-18 15:36:01 +0100

    Added circleci

--- app/main/routes.py
+++ app/main/routes.py
@@ -94,14 +94,14 @@ def follow(username):
     """
     Route for following user
     """
-    user = User.query.filter_by(username=username).first()
-    if user is None:
+    user_ = User.query.filter_by(username=username).first()
+    if user_ is None:
         flash('User {} not found.'.format(username))
         return redirect(url_for('index'))
-    if user == current_user:
+    if user_ == current_user:
         flash('You cannot follow yourself!')
         return redirect(url_for('user', username=username))
-    current_user.follow(user)
+    current_user.follow(user_)
     db.session.commit()
     flash('You are following {}!'.format(username))
     return redirect(url_for('user', username=username))
@@ -112,14 +112,14 @@ def unfollow(username):
     """
     Route for unfollowing user
     """
-    user = User.query.filter_by(username=username).first()
-    if user is None:
+    user_ = User.query.filter_by(username=username).first()
+    if user_ is None:
         flash('User {} not found.'.format(username))
         return redirect(url_for('index'))
-    if user == current_user:
+    if user_ == current_user:
         flash('You cannot unfollow yourself!')
         return redirect(url_for('user', username=username))
-    current_user.unfollow(user)
+    current_user.unfollow(user_)
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
